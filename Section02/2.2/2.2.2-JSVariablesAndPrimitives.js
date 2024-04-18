a667f427f20a7ae990c85a5a612298162f83b0af
Ethan Krieg <ethankrieg10@gmail.com>
2024-04-17 18:25:31 -0700

    2.2 Activities

---
+++ Section02/2.2/2.2.2-JSVariablesAndPrimitives.js
@@ -0,0 +1,46 @@
+// create a variable called "name" that references a string
+let name = "Ethan";
+
+// create a variable called "favoriteSong" that references a string
+let favoriteSong = "Empire State of Mind";
+
+// create a variable called "wage" that references a number
+let wage = 60;
+
+// create a variable called "age" that references a number
+let age = 23;
+
+// create a variable called "onlyChild" that references a boolean
+let onlyChild = false;
+
+// create a variable called "satisfied" that references a boolean
+let satisfied = false;
+
+// create a variable called "favoriteThing" that references ANY primitive value
+// print the data type of "favoriteThing"
+let favoriteThing = "Sleep";
+console.log(favoriteThing);
+
+// You heard a new song that became your favorite.
+// Reassign the variable "favoriteSong"
+favoriteSong = "Under the Bridge";
+
+// You no longer like the type of data stored in "favoriteThing"
+// Reassign "favoriteThing" to a different primitive data type and print its type
+favoriteThing = 14;
+console.log(favoriteThing);
+
+// Happy Birthday!
+// Reassign "age" to a new value
+age = 24;
+
+// You got a big raise at work.
+// Reassign "wage" to a new value
+wage = 65;
+
+// Your raise changed your job satisfaction.
+// Assign "satisfied" to true
+satisfied = true;
+
+// use "console.log()" and "+" to print a sentence that includes two of the above variables
+console.log("Hello, my name is " + name + ", and my salary is " + wage + ".");
